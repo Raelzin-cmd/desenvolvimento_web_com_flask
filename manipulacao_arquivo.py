a0c72f5a9214b757666f23c8c058fa43fbcb1062
Israel Almeida <almeidev.rael@gmail.com>
2025-11-27 18:08:38 -0300

    Exclusão de arquivos nao importante

--- manipulacao_arquivo.py
+++
@@ -1,8 +0,0 @@
-with open('teste.txt', 'r') as arquivo:
-    linha = arquivo.readline()
-    while linha:
-        print(linha)
-        linha = arquivo.readline()
-
-with open('teste.txt', 'a') as arquivo:
-    arquivo.write('\nis(Rael)')
