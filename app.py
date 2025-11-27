6ccafd2710c7e10dc8c38fd96afc84bb3a3028ef
Israel Almeida <almeidev.rael@gmail.com>
2025-11-27 15:49:57 -0300

    Adicionado a rota de detalhamento de carro

--- app.py
+++ app.py
@@ -10,22 +10,44 @@ servidor = Flask(__name__)
 # Dicionário = Objeto de carros
 carros = [
     {
+        'id': 1,
         'marca': 'Ford',
         'modelo': 'Ranger',
         'ano': 2020,
         'cor': 'Preto'
     },
     {
-        'marca': 'Chevolet',
+        'id': 2,
+        'marca': 'Chevrolet',
         'modelo': 'Camaro',
         'ano': 2023,
         'cor': 'Prata'
     }
 ]
 
+
+# Listar                   # http://localhost:5001/carros
 @servidor.route('/carros', methods=['GET'])
 def lista_carros(): # make_response para configurar o status (200 é o padrão)
-    return make_response(jsonify(carros))  # Assegurar que será retornado uma lista json     
+    return make_response(jsonify(carros))  # Assegurar que será retornado uma lista json
+
+
+# Detalhar               Convertendo (string) para (int)    # http://localhost:5001/carros/1
+@servidor.route('/carros/<int:id_carro>', methods=['GET'])
+def detalhe_carros(id_carro):   # id_carro recebe o ID mencionado na URL
+    # Percorre toda a lista de carros
+    for item in carros:
+        # Compara o ID da procura na URL com o ID dos carros no código
+        if item['id'] == id_carro:
+            # Retorna os detalhes do carro se o ID encontrado
+            return make_response(jsonify(item))
+        
+    # Status de erro se o ID não for encontrado
+    return make_response(jsonify({'message': 'O carro não existe'}), 404)
+
+
+
+
 
 # Alterando porta de saída padrão, nome do IP e ativando modo debug
 servidor.run(port=5001, host='localhost', debug=True)
@@ -34,4 +56,4 @@ servidor.run(port=5001, host='localhost', debug=True)
 
 
 
-# URL para usar no postman ou navegador http://localhost:5001/carros
+# Use no postman ou insomnia para testar requisições da URL http://localhost:5001/carros
